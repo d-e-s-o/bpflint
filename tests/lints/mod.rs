5eff923accd9606a850f308108fcd322917a8ac3
Daniel Müller <deso@posteo.net>
2025-08-15 09:00:53 -0700

    get_current_task

--- tests/lints/mod.rs
+++ tests/lints/mod.rs
@@ -5,6 +5,8 @@ mod validate;
 
 // Tests for individual lints go below here.
 
+#[path = "get-current-task.rs"]
+mod get_current_task;
 #[path = "probe-read.rs"]
 mod probe_read;
 #[path = "unstable-attach-point.rs"]
