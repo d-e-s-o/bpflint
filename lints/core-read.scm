4006ccba19a2b08b57ec234e798ad025637c8786
Matt St. Hilaire <mattsthilaire@meta.com>
2025-11-25 16:04:54 -0600

    Add user kernel version as command line arg

--- lints/core-read.scm
+++ lints/core-read.scm
@@ -1,3 +1,4 @@
 (call_expression
     function: (identifier) @function (#any-of? @function "bpf_core_read" "BPF_CORE_READ")
+    (#set! "min_kernel_version" "5.2.0")
 )
--- lints/get-current-task.scm
+++ lints/get-current-task.scm
@@ -1,4 +1,5 @@
 (call_expression
     function: (identifier) @function (#eq? @function "bpf_get_current_task")
     arguments: (argument_list) @__args (#match? @__args "^\\(\\s*\\)$")
+    (#set! "min_kernel_version" "5.11.0")
 )
